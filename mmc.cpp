3051fd9b1a38fa718a9b9710474f9e5312820a91
Anja Kalaba <akalaba@andrew.cmu.edu>
2019-11-12 20:34:47 -0500

    fixed segfault, added casing on size of register so GetContextReg doesn't fail

--- mmc.cpp
+++ mmc.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <map>
 #include <iostream>
+#include <stdbool.h>
 using namespace std;
 
 // A big table for memory
@@ -14,28 +15,40 @@ FILE *trace;
 ADDRINT DoLoad(REG reg, ADDRINT *addr) {
     fprintf(trace, "\nEmulate loading from addr %p to %s\n", addr,
             REG_StringShort(reg).c_str());
-    ADDRINT value;
-    PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
-    //value = memory.find(addr)->second;
+    ADDRINT value = 9;
+    // PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
+    // value = memory.find(addr)->second;
 
-    fprintf(trace, "\nvalue = %d\n", (int)value);
+    // fprintf(trace, "\nvalue = %d\n", (int)value);
     return value;
 }
 
 ADDRINT DoStore(CONTEXT *ctxt, REG reg, ADDRINT *addr) {
-    cout << "reg value: " << PIN_GetContextReg(ctxt, reg) << "\n";
-    cout << "addr " << addr << " register " << REG_StringShort(reg).c_str() << "\n";
+    cout << "HI\n";
+    cout << "addr " << addr << " register " << REG_StringShort(reg).c_str()
+         << "\n";
     fprintf(trace, "\nEmulate storing TO %p from %s\n", addr,
             REG_StringShort(reg).c_str());
-    //ADDRINT value;
-    UINT8 value;
+    ADDRINT value;
 
-    //PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
-    PIN_GetContextRegval(ctxt, reg, &value);
-    cout << "reg val " << value << "\n";
-    fprintf(trace, "\nvalue: %d\n", value);
+    cout << "REg" << reg << "\n";
+    UINT32 rw = REG_Size(reg);
+    cout << "width = " << rw << "\n";
+    if (rw < 8)
+        value = PIN_GetContextReg(ctxt, REG_FullRegName(reg));
+    else
+        value = PIN_GetContextReg(ctxt, reg);
+    if (rw == 2)
+        value &= 0xFFFF;
+    else if (rw == 4)
+        value &= 0xFFFFFFFF;
 
-    //memory.insert(make_pair(addr, value));
+    // value = PIN_GetContextReg(ctxt, reg);
+    cout << "reg val from ContextRegval" << value << "\n";
+    // fprintf(trace, "\nvalue: %d\n", value);
+
+    cout << "BYE\n";
+    // memory.insert(make_pair(addr, value));
     return value;
 }
 
@@ -54,26 +67,26 @@ VOID EmulateLoad(INS ins, VOID *v) {
     }*/
 
     // Find the instructions that move a value from memory to a register
-    /*if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryRead(ins) &&
+    if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryRead(ins) &&
         INS_OperandIsReg(ins, 0) && INS_OperandIsMemory(ins, 1)) {
         // op0 <- *op1
         fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
-        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad), IARG_UINT32,
+        /*INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad), IARG_UINT32,
                        REG(INS_OperandReg(ins, 0)), IARG_MEMORYREAD_EA,
                        IARG_RETURN_REGS, INS_OperandReg(ins, 0), IARG_END);
         // Delete the instruction
-        INS_Delete(ins);
-    }*/
+        INS_Delete(ins);*/
+    }
     // moves value from register to memory (store)
     if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryWrite(ins) &&
         INS_OperandIsReg(ins, 1) && INS_OperandIsMemory(ins, 0)) {
         fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
-        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoStore), IARG_CONTEXT, IARG_UINT32,
-                       REG(INS_OperandReg(ins, 1)), IARG_MEMORYWRITE_EA,
-                       IARG_RETURN_REGS, INS_OperandReg(ins, 1),
-                       IARG_END);
+        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoStore), IARG_CONTEXT,
+                       IARG_UINT32, REG(INS_OperandReg(ins, 1)),
+                       IARG_MEMORYWRITE_EA, IARG_RETURN_REGS,
+                       INS_OperandReg(ins, 1), IARG_END);
         // Delete the instruction
-        INS_Delete(ins);
+        // INS_Delete(ins);
     }
 }
 
