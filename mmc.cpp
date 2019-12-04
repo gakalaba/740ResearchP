e7adb903618f6a4d647336e46ad15aedcf3d635b
Anja Kalaba <akalaba@andrew.cmu.edu>
2019-12-03 19:18:18 -0500

    what we have so far...?

--- mmc.cpp
+++ mmc.cpp
@@ -6,34 +6,48 @@
 using namespace std;
 
 // A big table for memory
-map<long unsigned int *, long unsigned int> memory;
-
+map <ADDRINT *, ADDRINT> memory;
 // Initalize all objects
+
+int total = 0;
 
 FILE *trace;
 
-ADDRINT DoLoad(REG reg, ADDRINT *addr) {
-    fprintf(trace, "\nEmulate loading from addr %p to %s\n", addr,
-            REG_StringShort(reg).c_str());
-    ADDRINT value = 9;
-    // PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
-    // value = memory.find(addr)->second;
+void print_mem() {
+    if (total > 100) return;
+    total++;
+    cout << "printing map:\n";
+    map<long unsigned int *, long unsigned int>::iterator it;
+    for (it = memory.begin(); it != memory.end(); it++) {
+        cout << "addr = " << it->first << "   value = " << it->second << "\n";
+    }
+    cout << "\n";
+    return;
+}
 
-    // fprintf(trace, "\nvalue = %d\n", (int)value);
+ADDRINT DoLoad(REG reg, ADDRINT *addr) {
+    // print_mem();
+    ADDRINT value;
+    map<long unsigned int *, long unsigned int>::iterator it =
+        memory.find(addr);
+    PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
+        cout << "SafeCopy " << addr << " with value " << value << "\n";
+    if (it != memory.end()) {
+        value = it->second;
+        cout << "FOUND " << addr << " with value " << value << "\n";
+    }
+
+    fprintf(trace, "\nEmulate loading %d from addr %p to %s\n", (int)value,
+            addr, REG_StringShort(reg).c_str());
+
     return value;
 }
 
 ADDRINT DoStore(CONTEXT *ctxt, REG reg, ADDRINT *addr) {
-    cout << "HI\n";
-    cout << "addr " << addr << " register " << REG_StringShort(reg).c_str()
-         << "\n";
-    fprintf(trace, "\nEmulate storing TO %p from %s\n", addr,
-            REG_StringShort(reg).c_str());
+    // print_mem();
     ADDRINT value;
 
-    cout << "REg" << reg << "\n";
     UINT32 rw = REG_Size(reg);
-    cout << "width = " << rw << "\n";
     if (rw < 8)
         value = PIN_GetContextReg(ctxt, REG_FullRegName(reg));
     else
@@ -42,45 +56,33 @@ ADDRINT DoStore(CONTEXT *ctxt, REG reg, ADDRINT *addr) {
         value &= 0xFFFF;
     else if (rw == 4)
         value &= 0xFFFFFFFF;
-
-    // value = PIN_GetContextReg(ctxt, reg);
-    cout << "reg val from ContextRegval" << value << "\n";
-    // fprintf(trace, "\nvalue: %d\n", value);
-
-    cout << "BYE\n";
-    // memory.insert(make_pair(addr, value));
+    fprintf(trace, "\nEmulate storing %d TO %p from %s\n", (int)value, addr,
+            REG_StringShort(reg).c_str());
+    memory.insert(make_pair(addr, value));
+    print_mem();
     return value;
 }
 
 ////=======================================================
 //// Instrumentation routines
 ////=======================================================
-VOID EmulateLoad(INS ins, VOID *v) {
-    /*UINT32 memOperands = INS_MemoryOperandCount(ins);
-    for (UINT32 memOp = 0; memOp < memOperands; memOp++) {
-        if (INS_MemoryOperandIsRead(ins, memOp)) {
-            // Run cacheRoutine for Data (load) cache
-            INS_InsertPredicatedCall(ins, IPOINT_BEFORE,
-                                     (AFUNPTR)callDataReadRoutine,
-                                     IARG_MEMORYOP_EA, memOp, IARG_END);
-        }
-    }*/
+VOID EmulateLoadStore(INS ins, VOID *v) {
 
     // Find the instructions that move a value from memory to a register
     if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryRead(ins) &&
         INS_OperandIsReg(ins, 0) && INS_OperandIsMemory(ins, 1)) {
         // op0 <- *op1
-        fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
-        /*INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad), IARG_UINT32,
+        // fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
+        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad), IARG_UINT32,
                        REG(INS_OperandReg(ins, 0)), IARG_MEMORYREAD_EA,
                        IARG_RETURN_REGS, INS_OperandReg(ins, 0), IARG_END);
         // Delete the instruction
-        INS_Delete(ins);*/
+        INS_Delete(ins);
     }
     // moves value from register to memory (store)
     if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryWrite(ins) &&
         INS_OperandIsReg(ins, 1) && INS_OperandIsMemory(ins, 0)) {
-        fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
+        // fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
         INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoStore), IARG_CONTEXT,
                        IARG_UINT32, REG(INS_OperandReg(ins, 1)),
                        IARG_MEMORYWRITE_EA, IARG_RETURN_REGS,
@@ -118,7 +120,10 @@ int main(int argc, char *argv[]) {
 
     trace = fopen("pinatrace.out", "w");
 
-    INS_AddInstrumentFunction(EmulateLoad, 0);
+    // Register ImageLoad to be called when each image is loaded.
+    IMG_AddInstrumentFunction(ImageLoad, 0);
+
+    INS_AddInstrumentFunction(EmulateLoadStore, 0);
     PIN_AddFiniFunction(Fini, 0);
 
     // Never returns
