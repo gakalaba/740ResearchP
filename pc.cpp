82bbb874d759207e9a4b591631de18ef1c325ac9
Anja Kalaba <akalaba@andrew.cmu.edu>
2019-12-05 23:05:25 -0500

    added locking, ported over changes from pc to sc and tso, renamed mmc.cpp to sc.cpp, still thinking about last race condition on thread_fork and looking at live array

--- pc.cpp
+++ pc.cpp
@@ -15,7 +15,7 @@ using namespace std;
 struct mem_elem *memory[MAX_THREADS];
 
 struct mem_elem {
-    ADDRINT *addr; // key
+    ADDRINT *addr;  // key
     ADDRINT val;
     UT_hash_handle hh;
 };
@@ -26,41 +26,47 @@ struct queue_elem {
     UINT64 cycle;
 };
 
-struct mem_queue_elem {
-    ADDRINT val;
-    UINT64 cycle;
-    THREADID tid;
-};
-
 struct pending_addr {
     ADDRINT *addr;
     UINT32 size;
 };
 
 // array of write queues per thread
-queue <struct queue_elem> write_qs[MAX_THREADS];
+queue<struct queue_elem> write_qs[MAX_THREADS];
+
 bool thread_alive[MAX_THREADS];
+PIN_LOCK wr_locks[MAX_THREADS];
 
 // Initalize all objects
-PIN_LOCK ins_lock;
+PIN_LOCK main_mem_lock;
 int total = 0;
 bool in_main = false;
 FILE *trace;
 UINT64 ins_count[MAX_THREADS];
 THREADID main_tid;
 
-struct pending_addr *pending_addrs[MAX_THREADS];
+struct pending_addr pending_addrs[MAX_THREADS];
 
 void add_store(ADDRINT *addr, ADDRINT val, THREADID tid) {
     struct mem_elem *me;
     HASH_FIND_INT(memory[(tid % MAX_THREADS)], &addr, me);
-    if (me == NULL) {
-        me = (struct mem_elem *)malloc(sizeof(mem_elem));
-        me -> addr = addr;
-        me -> val = val;
-        HASH_ADD_INT(memory[(tid % MAX_THREADS)], addr, me);
+    struct mem_elem *new_me = (struct mem_elem *)malloc(sizeof(mem_elem));
+    if (tid == main_tid) {
+        PIN_GetLock(&main_mem_lock, 1);
+    }
+    if (me != NULL) {
+        me->val = val;
+        if (tid == main_tid) {
+            PIN_ReleaseLock(&main_mem_lock, 1);
+        }
+        free(new_me);
     } else {
-        me -> val = val;
+        new_me->addr = addr;
+        new_me->val = val;
+        HASH_ADD_INT(memory[(tid % MAX_THREADS)], addr, new_me);
+        if (tid == main_tid) {
+            PIN_ReleaseLock(&main_mem_lock, 1);
+        }
     }
 }
 
@@ -70,7 +76,7 @@ int get_load(ADDRINT *addr, ADDRINT *value, THREADID tid) {
     if (me == NULL) {
         return -1;
     }
-    *value = me -> val;
+    *value = me->val;
     return 0;
 }
 
@@ -78,9 +84,10 @@ void print_mem() {
     if (total > 100) return;
     total++;
     cout << "printing map:\n";
-    map<long unsigned int *, long unsigned int>::iterator it;
-    for (it = memory.begin(); it != memory.end(); it++) {
-        cout << "addr = " << it->first << "   value = " << it->second << "\n";
+    struct mem_elem *me;
+    struct mem_elem *src = memory[(main_tid % MAX_THREADS)];
+    for (me = src; me != NULL; (struct mem_elem *)(me->hh.next)) {
+        cout << "addr = " << src->addr << "   value = " << src->val << "\n";
     }
     cout << "\n";
     return;
@@ -96,9 +103,11 @@ void copy_memory(THREADID tid) {
     struct mem_elem *dst = memory[(tid % MAX_THREADS)];
     struct mem_elem *me;
     dst = NULL;
-    for(me = src; me != NULL; me = (struct mem_elem *)(me->hh.next)) {
-        add_store(src -> addr, src -> val, tid);
+    PIN_GetLock(&main_mem_lock, 1);
+    for (me = src; me != NULL; me = (struct mem_elem *)(me->hh.next)) {
+        add_store(src->addr, src->val, tid);
     }
+    PIN_ReleaseLock(&main_mem_lock, 1);
 }
 
 ADDRINT DoLoad1(ADDRINT *addr, UINT32 size, THREADID tid) {
@@ -106,15 +115,14 @@ ADDRINT DoLoad1(ADDRINT *addr, UINT32 size, THREADID tid) {
     ADDRINT value;
 
     // check if it's in our hashmap
-    if(get_load(addr, &value, tid) < 0) {
-        //PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
+    if (get_load(addr, &value, tid) < 0) {
+        // PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
         value = get_val((*addr), size);
     } else {
         value = get_val(value, size);
     }
 
-    fprintf(trace, "\nEmulate loading %d from addr %p\n", (int)value,
-            addr);
+    fprintf(trace, "\nEmulate loading %d from addr %p\n", (int)value, addr);
 
     return value;
 }
@@ -124,15 +132,15 @@ VOID DoLoad2(ADDRINT *addr1, ADDRINT *addr2, UINT32 size, THREADID tid) {
     ADDRINT value1, value2;
 
     // check if it's in our hashmap
-    if(get_load(addr1, &value1, tid) < 0) {
-        //PIN_SafeCopy(&value1, addr1, sizeof(ADDRINT));
+    if (get_load(addr1, &value1, tid) < 0) {
+        // PIN_SafeCopy(&value1, addr1, sizeof(ADDRINT));
         value1 = get_val((*addr1), size);
     } else {
         value1 = get_val(value1, size);
     }
 
-    if(get_load(addr2, &value2, tid) < 0) {
-        //PIN_SafeCopy(&value2, addr2, sizeof(ADDRINT));
+    if (get_load(addr2, &value2, tid) < 0) {
+        // PIN_SafeCopy(&value2, addr2, sizeof(ADDRINT));
         value2 = get_val((*addr2), size);
     } else {
         value2 = get_val(value2, size);
@@ -141,47 +149,53 @@ VOID DoLoad2(ADDRINT *addr1, ADDRINT *addr2, UINT32 size, THREADID tid) {
     fprintf(trace, "\nEmulate loading 2 vals: %d from addr %p  %d from addr
             %p\n", (int)value1, addr1, (int)value2, addr2);
 
-    //return value;
+    // return value;
 }
 
 UINT64 get_base(int tid_index) {
-    queue <struct queue_elem> write_q = write_qs[tid_index];
+    queue<struct queue_elem> write_q = write_qs[tid_index];
+    PIN_GetLock(&wr_locks[tid_index], 1);
     if (write_q.empty()) {
+        PIN_ReleaseLock(&wr_locks[tid_index], 1);
         return ins_count[tid_index];
     }
     // must preserve write-write ordering
     struct queue_elem e = write_q.back();
-    return e.cycle + 1;
+    UINT64 c = e.cycle + 1;
+    PIN_ReleaseLock(&wr_locks[tid_index], 1);
+    return c;
 }
 
 VOID add_to_queues(ADDRINT *addr, ADDRINT value, UINT64 write_delay) {
-    for(int i = 0; i < MAX_THREADS; i++) {
+    for (int i = 0; i < MAX_THREADS; i++) {
         if (thread_alive(i)) {
-             // Queue the write
+            // Queue the write
             UINT64 last_time = get_base(i);
             UINT64 network_delay = (UINT64)(rand() % MAX_NET_DELAY);
             UINT64 pop_cycle = last_time + write_delay + network_delay;
 
             struct queue_elem *e = malloc(sizeof(struct queue_elem));
-            e -> addr = addr;
-            e -> val = value;
-            e -> cycle = pop_cycle;
+            e->addr = addr;
+            e->val = value;
+            e->cycle = pop_cycle;
 
             // lock
-            queue <struct queue_elem> q = write_qs[i];
+            queue<struct queue_elem> q = write_qs[i];
+            PIN_GetLock(&wr_locks[i], 1);
             q.push(e);
+            PIN_ReleaseLock(&wr_locks[i], 1);
         }
     }
 }
 
 VOID BeforeStore(ADDRINT *addr, UINT32 size, THREADID tid) {
-    pending_addrs[(tid % MAX_THREADS)] -> addr = addr;
-    pending_addrs[(tid % MAX_THREADS)] -> size = size;
+    pending_addrs[(tid % MAX_THREADS)].addr = addr;
+    pending_addrs[(tid % MAX_THREADS)].size = size;
 }
 
 VOID AfterStore(THREADID tid) {
-    ADDRINT *addr = pending_addrs[(tid % MAX_THREADS)] -> addr;
-    UINT32 size = pending_addrs[(tid % MAX_THREADS)] -> size;
+    ADDRINT *addr = pending_addrs[(tid % MAX_THREADS)].addr;
+    UINT32 size = pending_addrs[(tid % MAX_THREADS)].size;
 
     ADDRINT value = get_val((*addr), size);
 
@@ -190,85 +204,100 @@ VOID AfterStore(THREADID tid) {
     UINT64 pop_cycle = (UINT64)(rand() % MAX_DELAY) + last_time;
 
     struct queue_elem *e = malloc(sizeof(struct queue_elem));
-    e -> addr = addr;
-    e -> val = value;
-    e -> cycle = pop_cycle;
+    e->addr = addr;
+    e->val = value;
+    e->cycle = pop_cycle;
 
     // lock
-    queue <struct queue_elem> q = write_qs[(tid % MAX_THREADS)];
+    queue<struct queue_elem> q = write_qs[(tid % MAX_THREADS)];
+    PIN_GetLock(&wr_locks[(tid % MAX_THREADS)], 1);
     q.push(e);
+    PIN_ReleaseLock(&wr_locks[(tid % MAX_THREADS)], 1);
 
     // PC - add to everyone's queue
     add_to_queues(addr, value, (pop_cycle - last_time));
 
-    //return value;
+    // return value;
 }
 
 VOID ProcessQueue(THREADID tid) {
-    PIN_GetLock(&ins_lock, tid);
     ins_count[(tid % MAX_THREADS)]++;
-    PIN_ReleaseLock(&ins_lock);
-    queue <struct queue_elem> write_q = write_qs[(tid % MAX_THREADS)];
+    queue<struct queue_elem> write_q = write_qs[(tid % MAX_THREADS)];
+    PIN_GetLock(&wr_locks[(tid % MAX_THREADS)], 1);
     if (!write_q.empty()) {
         struct queue_elem e = write_q.front();
         if (ins_count[(tid % MAX_THREADS)] >= e.cycle) {
-            // DO THE WRITE
-            add_store(e->addr, e->val, tid);
             // Take it out of the queue
             write_q.pop();
+            PIN_ReleaseLock(&wr_locks[(tid % MAX_THREADS)], 1);
+            // DO THE WRITE
+            add_store(e.addr, e.val, tid);
+            free(e);
+            return;
         }
     }
+    PIN_ReleaseLock(&wr_locks[(tid % MAX_THREADS)], 1);
 }
 
 VOID FlushQueue(THREADID tid) {
-    queue <struct queue_elem> write_q = write_qs[(tid % MAX_THREADS)];
+    queue<struct queue_elem> write_q = write_qs[(tid % MAX_THREADS)];
+    PIN_GetLock(&wr_locks[(tid % MAX_THREADS)], 1);
     if (!write_q.empty()) {
         struct queue_elem e = write_q.front();
-        // DO THE WRITE
-        add_store(e->addr, e->val, tid);
         // Take it out of the queue
         write_q.pop();
+        PIN_ReleaseLock(&wr_locks[(tid % MAX_THREADS)], 1);
+        // DO THE WRITE
+        add_store(e.addr, e.val, tid);
+        // Take it out of the queue
+        write_q.pop();
+        free(e);
+        return;
     }
+    PIN_ReleaseLock(&wr_locks[(tid % MAX_THREADS)], 1);
 }
 
 ////=======================================================
 //// Instrumentation routines
 ////=======================================================
 VOID EmulateLoadStore(INS ins, VOID *v) {
-    if(in_main) {
-        if(INS_IsAtomicUpdate(ins)) {
+    if (in_main) {
+        // Atomic updates must always see the most recent version,
+        // so the queue must be flushed in order to see this
+        if (INS_IsAtomicUpdate(ins)) {
             INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(FlushQueue),
-                IARG_THREAD_ID, IARG_END);
+                           IARG_THREAD_ID, IARG_END);
         }
 
         INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(ProcessQueue),
-                IARG_THREAD_ID, IARG_END);
+                       IARG_THREAD_ID, IARG_END);
         // Find the instructions that move a value from memory to a register
         if (INS_IsMemoryRead(ins)) {
             // op0 <- *op1
             // fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
             INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad1), IARG_UINT32,
-                       IARG_MEMORYREAD_EA, IARG_MEMORYREAD_SIZE, IARG_END);
+                           IARG_MEMORYREAD_EA, IARG_MEMORYREAD_SIZE, IARG_END);
             // Delete the instruction
-            //INS_Delete(ins);
+            // INS_Delete(ins);
         }
         if (INS_HasMemoryRead2(ins)) {
             // op0 <- *op1
             // fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
             INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad2), IARG_UINT32,
-                       IARG_MEMORYREAD_EA, IARG_MEMORYREAD2_EA,
-                       IARG_MEMORYREAD_SIZE, IARG_END);
+                           IARG_MEMORYREAD_EA, IARG_MEMORYREAD2_EA,
+                           IARG_MEMORYREAD_SIZE, IARG_END);
             // Delete the instruction
-            //INS_Delete(ins);
+            // INS_Delete(ins);
         }
-// moves value from register to memory (store)
+        // moves value from register to memory (store)
         if (INS_IsMemoryWrite(ins)) {
             // fprintf(trace, "\n%s\n", (INS_Disassemble(ins)).c_str());
-            INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoStore),
-                    IARG_UINT32, IARG_MEMORYWRITE_EA, IARG_MEMORYWRITE_SIZE,
-                    IARG_END);
-        // Delete the instruction
-        // INS_Delete(ins);
+            INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(BeforeStore),
+                           IARG_UINT32, IARG_MEMORYWRITE_EA,
+                           IARG_MEMORYWRITE_SIZE, IARG_THREAD_ID, IARG_END);
+
+            INS_InsertCall(ins, IPOINT_AFTER, AFUNPTR(AfterStore),
+                           IARG_THREAD_ID, IARG_END);
         }
     }
 }
@@ -276,6 +305,7 @@ VOID EmulateLoadStore(INS ins, VOID *v) {
 VOID BeforeMain(int size, THREADID threadid) {
     // program shouldn't be multithreaded when we hit main
     in_main = true;
+    main_tid = threadid;
     cout << "in main\n";
 }
 
@@ -287,13 +317,14 @@ VOID AfterMain(ADDRINT ret) {
 VOID ImageLoad(IMG img, VOID *) {
     RTN rtn = RTN_FindByName(img, "main");
 
-    if ( RTN_Valid( rtn )) {
+    if (RTN_Valid(rtn)) {
         RTN_Open(rtn);
 
         RTN_InsertCall(rtn, IPOINT_BEFORE, AFUNPTR(BeforeMain),
-            IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_THREAD_ID, IARG_END);
+                       IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_THREAD_ID,
+                       IARG_END);
         RTN_InsertCall(rtn, IPOINT_AFTER, AFUNPTR(AfterMain),
-            IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+                       IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
 
         RTN_Close(rtn);
     }
@@ -302,6 +333,23 @@ VOID ImageLoad(IMG img, VOID *) {
 VOID Fini(INT32 code, VOID *v) {
     fprintf(trace, "#eof\n");
     fclose(trace);
+    struct mem_elem *me, next_me;
+    for (int i = 0; i < MAX_THREADS; i++) {
+        me = memory[i];
+        while (me != NULL) {
+            next_me = (struct mem_elem *)(me->hh.next);
+            free(me);
+            me = next_me
+        }
+    }
+}
+
+VOID ThreadStart(THREADID threadid, CONTEXT *ctxt, INT32 flags, VOID *v) {
+    thread_alive[(threadid % MAX_THREADS)] = true;
+}
+
+VOID ThreadFini(THREADID threadid, const CONTEXT *ctxt, INT32 code, VOID *v) {
+    thread_alive[(threadid % MAX_THREADS)] = false;
 }
 
 /* =====================================================================
@@ -309,8 +357,8 @@ VOID Fini(INT32 code, VOID *v) {
 /* Print Help Message */
 /* =====================================================================
  */
-
-INT32 Usage() {
+INT32
+Usage() {
     PIN_ERROR("This Pintool prints a trace of memory addresses\n" +
               KNOB_BASE::StringKnobSummary() + "\n");
     return -1;
@@ -329,10 +377,18 @@ int main(int argc, char *argv[]) {
 
     trace = fopen("pinatrace.out", "w");
 
+    // Initialize thread array variables
+    for (int i = 0; i < MAX_THREADS; i++) {
+        thread_alive[i] = false;
+        ins_count[i] = 0;
+    }
     // Register ImageLoad to be called when each image is loaded.
     IMG_AddInstrumentFunction(ImageLoad, 0);
 
     INS_AddInstrumentFunction(EmulateLoadStore, 0);
+
+    PIN_AddThreadStartFunction(ThreadStart, 0);
+    PIN_AddThreadFiniFunction(ThreadFini, 0);
 
     PIN_AddFiniFunction(Fini, 0);
 
--- sc.cpp
+++ sc.cpp
@@ -1,22 +1,14 @@
 #include "pin.H"
 #include <stdio.h>
 #include <map>
-#include <cstdlib>
 #include <iostream>
 #include <stdbool.h>
-#include <ctime>
 #include "uthash/include/uthash.h"
 using namespace std;
 
-#define MAX_DELAY (20)
-#define MAX_NET_DELAY (10)
 #define MAX_THREADS (1024)
 
-struct queue_elem {
-    ADDRINT val;
-    ADDRINT *addr;
-    UINT64 cycle;
-};
+PIN_LOCK mem_lock;
 
 struct mem_elem {
     ADDRINT *addr;  // key
@@ -31,10 +23,8 @@ struct pending_addr {
 
 // A big table for memory
 struct mem_elem *memory;
-PIN_LOCK mem_lock;
-queue<struct queue_elem> write_qs[MAX_THREADS];
 struct pending_addr pending_addrs[MAX_THREADS];
-THREADID main_tid;
+// Initalize all objects
 
 void add_store(ADDRINT *addr, ADDRINT val, THREADID tid) {
     struct mem_elem *me;
@@ -46,59 +36,57 @@ void add_store(ADDRINT *addr, ADDRINT val, THREADID tid) {
         PIN_ReleaseLock(&mem_lock, tid);
         free(new_me);
     } else {
-        me->addr = addr;
-        me->val = val;
+        new_me->addr = addr;
+        new_me->val = val;
         HASH_ADD_INT(memory, addr, me);
         PIN_ReleaseLock(&mem_lock, tid);
     }
 }
 
-int get_load(ADDRINT *addr, ADDRINT *value, THREADID tid) {
+int read_map(ADDRINT *addr, ADDRINT *value) {
     struct mem_elem *me;
     HASH_FIND_INT(memory, &addr, me);
     PIN_GetLock(&mem_lock, 1);
     if (me == NULL) {
-        PIN_ReleaseLock(&mem_lock, tid);
+        PIN_ReleaseLock(&mem_lock, 1);
         return -1;
     }
     *value = me->val;
-    PIN_ReleaseLock(&mem_lock, tid);
+    PIN_ReleaseLock(&mem_lock, 1);
     return 0;
 }
 
-// Initalize all objects
-PIN_LOCK ins_lock;
 int total = 0;
 bool in_main = false;
 FILE *trace;
-UINT64 ins_count[MAX_THREADS];
 
 void print_mem() {
     if (total > 100) return;
     total++;
     cout << "printing map:\n";
     struct mem_elem *me;
+    struct mem_elem *src = memory;
     PIN_GetLock(&mem_lock, 1);
-    struct mem_elem *src = memory[(main_tid % MAX_THREADS)];
     for (me = src; me != NULL; (struct mem_elem *)(me->hh.next)) {
         cout << "addr = " << src->addr << "   value = " << src->val << "\n";
     }
-    PIN_ReleaseLock(&mem_lock, tid);
+    PIN_ReleaseLock(&mem_lock, 1);
     cout << "\n";
     return;
 }
 
-// We must always size the value from memory properly
 ADDRINT get_val(ADDRINT val, UINT32 size) {
     UINT32 mask = 0xFFFFFFFF << size;
     return ((~mask) & val);
 }
 
 ADDRINT DoLoad1(ADDRINT *addr, UINT32 size) {
+
+    // print_mem();
     ADDRINT value;
 
     // check if it's in our hashmap
-    if (get_load(addr, &value, tid) < 0) {
+    if (read_map(addr, &value) < 0) {
         // PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
         value = get_val((*addr), size);
     } else {
@@ -106,22 +94,23 @@ ADDRINT DoLoad1(ADDRINT *addr, UINT32 size) {
     }
 
     fprintf(trace, "\nEmulate loading %d from addr %p\n", (int)value, addr);
+
     return value;
 }
 
-VOID DoLoad2(ADDRINT *addr1, ADDRINT *addr2, UINT32 size) {
+ADDRINT DoLoad2(ADDRINT *addr1, ADDRINT *addr2, UINT32 size) {
     // print_mem();
     ADDRINT value1, value2;
 
     // check if it's in our hashmap
-    if (get_load(addr1, &value1, tid) < 0) {
+    if (read_map(addr1, &value1) < 0) {
         // PIN_SafeCopy(&value1, addr1, sizeof(ADDRINT));
         value1 = get_val((*addr1), size);
     } else {
         value1 = get_val(value1, size);
     }
 
-    if (get_load(addr2, &value2, tid) < 0) {
+    if (read_map(addr2, &value2) < 0) {
         // PIN_SafeCopy(&value2, addr2, sizeof(ADDRINT));
         value2 = get_val((*addr2), size);
     } else {
@@ -131,17 +120,7 @@ VOID DoLoad2(ADDRINT *addr1, ADDRINT *addr2, UINT32 size) {
     fprintf(trace, "\nEmulate loading 2 vals: %d from addr %p  %d from addr
             %p\n", (int)value1, addr1, (int)value2, addr2);
 
-    // return value;
-}
-
-UINT64 get_base(int tid_index) {
-    queue<struct queue_elem> write_q = write_qs[tid_index];
-    if (write_q.empty()) {
-        return ins_count[tid_index];
-    }
-    // must preserve write-write ordering
-    struct queue_elem e = write_q.back();
-    return e.cycle + 1;
+    return value;
 }
 
 VOID BeforeStore(ADDRINT *addr, UINT32 size, THREADID tid) {
@@ -155,36 +134,7 @@ VOID AfterStore(THREADID tid) {
 
     ADDRINT value = get_val((*addr), size);
 
-    // Queue the write
-    UINT64 last_time = get_base((tid % MAX_THREADS));
-    UINT64 write_delay = (UINT64)(rand() % MAX_DELAY);
-    UINT64 network_delay = (UINT64)(rand() % MAX_NET_DELAY);
-    // Still need network delay... because all the writes will see this
-    UINT64 pop_cycle = last_time + write_delay + network_delay;
-
-    struct queue_elem *e = malloc(sizeof(struct queue_elem));
-    e->addr = addr;
-    e->val = value;
-    e->cycle = pop_cycle;
-
-    // don't need lock, touching your own write queue without contention
-    queue<struct queue_elem> q = write_qs[(tid % MAX_THREADS)];
-    q.push(e);
-}
-
-VOID processQueue(THREADID tid) {
-    ins_count[(tid % MAX_THREADS)]++;
-    queue<struct queue_elem> write_q = write_qs[(tid % MAX_THREADS)];
-    if (!write_q.empty()) {
-        struct queue_elem e = write_q.front();
-        if (ins_count[(tid % MAX_THREADS)] >= e.cycle) {
-            // DO THE WRITE
-            add_store(e.addr, e.val, tid);
-            // Take it out of the queue
-            write_q.pop();
-            free(e);
-        }
-    }
+    add_store(addr, value)
 }
 
 ////=======================================================
@@ -192,9 +142,6 @@ VOID processQueue(THREADID tid) {
 ////=======================================================
 VOID EmulateLoadStore(INS ins, VOID *v) {
     if (in_main) {
-
-        INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(processQueue),
-                       IARG_THREAD_ID, IARG_END);
         // Find the instructions that move a value from memory to a register
         if (INS_IsMemoryRead(ins)) {
             // op0 <- *op1
@@ -229,7 +176,6 @@ VOID EmulateLoadStore(INS ins, VOID *v) {
 VOID BeforeMain(int size, THREADID threadid) {
     // program shouldn't be multithreaded when we hit main
     in_main = true;
-    main_tid = threadid;
     cout << "in main\n";
 }
 
@@ -289,21 +235,12 @@ INT32 Usage() {
 int main(int argc, char *argv[]) {
     if (PIN_Init(argc, argv)) return Usage();
 
-    srand(time(NULL));
-
     trace = fopen("pinatrace.out", "w");
-
-    // Initialize thread array variables
-    for (int i = 0; i < MAX_THREADS; i++) {
-        thread_alive[i] = false;
-        ins_count[i] = 0;
-    }
 
     // Register ImageLoad to be called when each image is loaded.
     IMG_AddInstrumentFunction(ImageLoad, 0);
 
-    INS_AddInstrumentFunction(EmulateLoadStore, 0);
-
+    // INS_AddInstrumentFunction(EmulateLoadStore, 0);
     PIN_AddFiniFunction(Fini, 0);
 
     // Never returns
